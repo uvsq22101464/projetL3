f9c2bd13568a2a6ea906e15ae207ae88140dddd9
Ethan MACHE <ethan.mache@gmail.com>
2024-04-01 18:35:00 +0200

    récup données Bedroom + affichage

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -21,11 +22,17 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
 public class MainActivity extends AppCompatActivity {
 
     Context context = this;
     // on définit table, le layout où on se trouve
-    LinearLayout table;
+    TableRow table_text;
+    TableRow table_button;
 
 
     @Override
@@ -34,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.accueil);
         DatabaseReference database = FirebaseDatabase.getInstance("https://projet-l3-maison-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
         // on cherche l'id de notre layout
-        table = (LinearLayout) findViewById(R.id.tableLayout);
+        table_text = (TableRow) findViewById(R.id.table_bedroom_text);
+        table_button = (TableRow) findViewById(R.id.table_bedroom_button);
         // on cherche le noeud à ajouter
         database.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
@@ -43,46 +51,45 @@ public class MainActivity extends AppCompatActivity {
                     Log.e("firebase", "Error getting data", task.getException());
                 }
                 else {
-                    String name = String.valueOf(task.getResult().getValue());
-                    Log.d("firebase", name);
-                    // row, on crée un nouveau TableRow. TextView, on crée ce qui contient le texte. on set le texte
-                    TableRow row = new TableRow(context);
-                    TextView txt = new TextView(context);
-                    Button button = new Button(context);
-                    txt.setText(name);
-                    button.setText(name);
-                    button.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent ia = new Intent(MainActivity.this, Manage_room.class);
-                            startActivity(ia);
+                    try {
+                        JSONObject data = new JSONObject(String.valueOf(task.getResult().getValue()));
+                        JSONObject bedroomData = data.getJSONObject("Bedroom");
+                        Iterator<String> bedroomNames = bedroomData.keys();
+                        // faire en sorte de d'abord récup toutes les données qu'on stock dans un tableau puis on itère dans ce tableau pour crée les bouttons
+                        while(bedroomNames.hasNext()) {
+                            String name = bedroomNames.next();
+                            Log.d("firebase", name);
+                            // on crée le textView et le bouton et on leur donne ce qu'ils contiennent
+                            TextView txt = new TextView(context);
+                            Button button = new Button(context);
+                            txt.setText(name);
+                            txt.setGravity(Gravity.CENTER);
+                            button.setText(name);
+                            button.setGravity(Gravity.CENTER);
+                            // on def le click du bouton
+                            button.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent ia = new Intent(MainActivity.this, Manage_room.class);
+                                    startActivity(ia);
+                                }
+                            });
+                            // on setup des paramètres pour les textes et les boutons
+                            TableRow.LayoutParams parameter = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.MATCH_PARENT);
+                            txt.setLayoutParams(parameter);
+                            button.setLayoutParams(parameter);
+
+                            // on ajoute dans la table parente
+                            table_text.addView(txt, parameter);
+                            table_button.addView(button, parameter);
                         }
-                    });
-                    // On def des paramètres pour la TableRow qu'on associe aussi au textView
-                    TableRow.LayoutParams parameter = new TableRow.LayoutParams(0, TableRow.LayoutParams.MATCH_PARENT, 1f);
-                    txt.setLayoutParams(parameter);
-                    button.setLayoutParams(parameter);
-                    // on ajoute le textView à la TableRow
-                    row.addView(txt);
-                    row.addView(button);
-                    // on ajoute dans la table la TableRow
-                    table.addView(row, new TableLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                    }catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
-        //DatabaseReference myRef = database.getReference("ch1");
-
-
         }
-
-
-
-
-
-
-
-
-
 
 
 
