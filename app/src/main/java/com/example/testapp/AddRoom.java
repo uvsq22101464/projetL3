2c1d8a1a00ba566064578f5821670d5b9188af2b
Ethan MACHE <ethan.mache@gmail.com>
2024-04-05 23:01:36 +0200

    modification page ajout de nouvelle salles

--- app/src/main/java/com/example/testapp/AddRoom.java
+++ app/src/main/java/com/example/testapp/AddRoom.java
@@ -3,11 +3,14 @@ package com.example.testapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TableLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +18,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class AddRoom extends AppCompatActivity {
 
     EditText roomName;
     Spinner type1;
-    Spinner type2;
+    ArrayList<Spinner> spinners;
 
 
     @Override
@@ -29,7 +34,7 @@ public class AddRoom extends AppCompatActivity {
 
         roomName = (EditText)findViewById(R.id.roomName);
         type1 = (Spinner)findViewById(R.id.spinner_rooms);
-        type2 = (Spinner)findViewById(R.id.spinner_captors);
+        spinners = new ArrayList<Spinner>();
 
         // Create an ArrayAdapter using the string array and a default spinner layout.
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(
@@ -41,62 +46,47 @@ public class AddRoom extends AppCompatActivity {
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner.
         type1.setAdapter(adapter1);
-
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
-                this,
-                R.array.captors,
-                android.R.layout.simple_spinner_item
-        );
-        // Specify the layout to use when the list of choices appears.
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner.
-        type2.setAdapter(adapter2);
-
     }
 
         public void createRoom(View v) {
+        // si la salle n'a pas de nom ça fout la merde
             String name = roomName.getText().toString();
             String room_type = (type1.getItemAtPosition(type1.getSelectedItemPosition()).toString());
-            String captor = (type2.getItemAtPosition(type2.getSelectedItemPosition()).toString());
-
-
-
-            // écrire dans la base de données
-
-
-            FirebaseDatabase database = FirebaseDatabase.getInstance("https://projet-l3-maison-default-rtdb.europe-west1.firebasedatabase.app/");
-            DatabaseReference myRef = database.getReference(room_type + "/" + name + "/" + captor);
-            // converti le type de capteur en la donnée qu'il prend
-            Toast.makeText(this, captor, Toast.LENGTH_SHORT).show();
-            if (captor.equals("Light")) {
-                myRef.setValue(false);
-            } else if (captor.equals("Temperature")) {
-                myRef.setValue(0.0);
-            } else {
-                myRef.setValue("Other");
+            for (Spinner spinner : spinners) {
+                String captor = (spinner.getItemAtPosition(spinner.getSelectedItemPosition()).toString());
+                FirebaseDatabase database = FirebaseDatabase.getInstance("https://projet-l3-maison-default-rtdb.europe-west1.firebasedatabase.app/");
+                DatabaseReference myRef = database.getReference(room_type + "/" + name + "/" + captor);
+                if (captor.equals("Light")) {
+                    myRef.setValue(false);
+                } else if (captor.equals("Temperature")) {
+                    myRef.setValue(0.0);
+                }
             }
-
-
             Intent ia = new Intent(this, Menu.class);
             startActivity(ia);
         }
 
 
-
-
-    //très probablement inutile, j'ai testé sans et ça à l'air de marcher
-    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
-
-        public void onItemSelected(AdapterView<?> parent, View view,
-                                   int pos, long id) {
-            // An item is selected. You can retrieve the selected item using
-            // parent.getItemAtPosition(pos).
-            parent.getItemAtPosition(pos);
+    public void addCaptor(View v) {
+        if (spinners.size() < getResources().getStringArray(R.array.captors).length - 1)  {
+            TableLayout table = findViewById(R.id.add_room_table);
+            Spinner spinner = new Spinner(this);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                    this,
+                    R.array.captors,
+                    android.R.layout.simple_spinner_item
+            );
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            // Apply the adapter to the spinner.
+            spinner.setAdapter(adapter);
+            spinners.add(spinner);
+            TableLayout.LayoutParams parameter = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT);
+            parameter.setMargins(0, 0, 0, 20);
+            spinner.setLayoutParams(parameter);
+            table.addView(spinner);
+        } else {
+            Toast.makeText(this, "Nombre maximum de capteurs atteint",Toast.LENGTH_SHORT).show();
         }
-
-        public void onNothingSelected(AdapterView<?> parent) {
-            // Another interface callback.
-        }
-
     }
 }
